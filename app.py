9b29176865ecdbed6ed4ebad64c2a19148e2f22c
Egan Done <edone@sympatico.ca>
2017-11-26 14:08:21 -0500

    Now dump out the environment variables on main request

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import os
+from io import StringIO
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "Hello, World!"
+def index():
+  out = StringIO()
+  out.write('<html><body>')
+  out.write('<h1>Hello, World!</h1>\n')
+  out.write('<pre>')
+  for param in os.environ.keys():
+    out.write("%40s %s\n" % (param,os.environ[param]))
+  out.write('</pre>')
+  out.write('</body></html>')
+  return out.getvalue()
 
 if __name__ == '__main__':
-  for param in os.environ.keys():
-    print("%20s %s" % (param, os.environ[param]))
   app.run(host="0.0.0.0", port=8080, debug=True)
